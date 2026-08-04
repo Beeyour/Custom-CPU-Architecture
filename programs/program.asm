37c4e0ec5ed192ca22429d1789fce5d49cd8bf6d
OMAR <beyour@outlook.sa>
2026-08-04 08:07:47 +0300

    test: update assembly test file and recompile memory.hex

--- programs/program.asm
+++ programs/program.asm
@@ -1,10 +1,19 @@
 ; === EXECUTION START ===
-LOAD AX, [word1]
-LOAD BX, [word2]
-STOR [word1 + 1], BX
+LOAD R0, [word1]
+MOVI R1, 1
+STOR [word2], R1
+MOVI R2, 0Fh
+STR [word2 + R0], R2
+LDR R3, [R0 + word2]
+MOVI SP, 100h
+PUSH R0
+PUSH R1
+POP R4
+POP R5
+POP R6
 HALT
-; === DATA SECTION ===
 
+; === DATA SECTION ===
 
 word1: dw FFh, 1fh
 word2: dw 32, 1010b
